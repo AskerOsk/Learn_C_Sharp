136293a4af6c96c67a5cfb326c188c91c599f64b
AskerOsk <oskar1999asker@gmail.com>
2024-08-08 22:32:00 +0500

    Трехмерный массив

--- Трехмерные массивы/Program.cs
+++ Трехмерные массивы/Program.cs
@@ -6,7 +6,55 @@ namespace Трехмерные_массивы
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[,,] myArray = new int [4, 7, 3];
+
+
+            /*            
+             *            // Пример как выглядит
+             *            {
+                            {
+                                {32, 45, 6 },
+                                {6, 1, 5 },
+                            },
+                            {
+                                {12, 55, 16 },
+                                {9, 5, 84}
+                            },
+                            {
+                                {2, 74, 523 },
+                                {4512, 45, 6354},
+                            }
+                        };
+            */
+
+            Random random = new Random();
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int x = 0; x < myArray.GetLength(2); x++)
+                    {
+                        myArray[i, j, x] = random.Next(100, 500);
+                    }
+                }
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int x = 0; x < myArray.GetLength(2); x++)
+                    {
+                        Console.Write(myArray[i, j, x] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("__");
+
+            }
+
         }
     }
 }
