6dc5b4c6c8a7442d97b996be06164fe3ced1a68f
AskerOsk <oskar1999asker@gmail.com>
2024-08-10 23:21:36 +0500

    Пройденные_уроки_без_кода

---
+++ Пройденные уроки без кода/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Пройденные_уроки_без_кода
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+                Class, struct, enum
+            */
+            Console.WriteLine("ССЫЛОЧНЫЕ И ЗНАЧИМЫЕ ТИПЫ C# | СТЕК И КУЧА C# | REFERENCE AND VALUE TYPES C# | C# Уроки | # 38 \nhttps://www.youtube.com/watch?v=_RUluZNOwfY&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=51");
+
+            Console.WriteLine("ОБЛАСТЬ ВИДИМОСТИ | КОНТЕКСТ ПЕРЕМЕННОЙ | КОНФЛИКТЫ ОБЛАСТЕЙ ВИДИМОСТИ | C# Уроки | # 37 \nhttps://www.youtube.com/watch?v=FyCp19W_5d8&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=50");
+
+        }
+    }
+}
