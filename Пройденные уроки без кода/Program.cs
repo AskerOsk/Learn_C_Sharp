4122f84e3c4153d9edaca5970839dce02972c3ab
AskerOsk <oskar1999asker@gmail.com>
2024-08-11 00:07:42 +0500

    ЧТО ТАКОЕ NULL

--- Пройденные уроки без кода/Program.cs
+++ Пройденные уроки без кода/Program.cs
@@ -12,6 +12,7 @@ namespace Пройденные_уроки_без_кода
             Console.WriteLine("ССЫЛОЧНЫЕ И ЗНАЧИМЫЕ ТИПЫ C# | СТЕК И КУЧА C# | REFERENCE AND VALUE TYPES C# | C# Уроки | # 38 \nhttps://www.youtube.com/watch?v=_RUluZNOwfY&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=51");
 
             Console.WriteLine("ОБЛАСТЬ ВИДИМОСТИ | КОНТЕКСТ ПЕРЕМЕННОЙ | КОНФЛИКТЫ ОБЛАСТЕЙ ВИДИМОСТИ | C# Уроки | # 37 \nhttps://www.youtube.com/watch?v=FyCp19W_5d8&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=50");
+            Console.WriteLine("ЧТО ТАКОЕ NULL | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 39 \nhttps://www.youtube.com/watch?v=wqUiYYVZDI4&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=52");
 
         }
     }
