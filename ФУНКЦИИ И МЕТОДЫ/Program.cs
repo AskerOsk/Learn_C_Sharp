f46940baee28e5be603b45f833cade8878321b15
AskerOsk <oskar1999asker@gmail.com>
2024-08-10 13:48:38 +0500

    ФУНКЦИИ_И_МЕТОДЫ__вывод_символов_в_консоль

---
+++ ФУНКЦИИ И МЕТОДЫ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ФУНКЦИИ_И_МЕТОДЫ
+{
+    class Program
+    {
+
+        static void Printline(string symbol, uint symbolCount)   // Если использовать char меняем тип.
+        {
+            for (int i = 0; i < symbolCount; i++)
+            {
+                Console.Write(symbol);
+            }
+        } 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ: ");
+            string symbol = Console.ReadLine();   // Если будем использовать char, то меняем ввод на -- char symbol = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("Введите количество символов: ");
+            uint symbolCount = uint.Parse(Console.ReadLine());
+
+            Printline(symbol, symbolCount);
+        }
+    }
+}
