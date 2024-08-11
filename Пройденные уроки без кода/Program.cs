7617f4683a61435c478c5d450f2f97c27f51304c
AskerOsk <oskar1999asker@gmail.com>
2024-08-11 14:26:52 +0500

    ОПЕРАТОР УСЛОВНОГО NULL - ?.

--- Пройденные уроки без кода/Program.cs
+++ Пройденные уроки без кода/Program.cs
@@ -15,7 +15,7 @@ namespace Пройденные_уроки_без_кода
             Console.WriteLine("ЧТО ТАКОЕ NULL | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 39 \nhttps://www.youtube.com/watch?v=wqUiYYVZDI4&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=52");
             Console.WriteLine("ОПЕРАТОР ОБЪЕДИНЕНИЯ С NULL С# | C# ?? | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 40 \n https://www.youtube.com/watch?v=mXh0ci6O6og&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=54");
             Console.WriteLine("ОПЕРАТОР ПРИСВАИВАНИЯ ОБЪЕДИНЕНИЯ СО ЗНАЧЕНИЕМ NULL в C# 8 | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 41\nhttps://www.youtube.com/watch?v=Jb-dFOXrSa0&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=54");
-            // Console.WriteLine(" \n");
+            Console.WriteLine("ОПЕРАТОР УСЛОВНОГО NULL | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 42 ?. \n https://www.youtube.com/watch?v=UwVYKGi0XJk&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=55");
             // Console.WriteLine(" \n");
 
 
