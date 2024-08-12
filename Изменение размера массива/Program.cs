8d377d450ae4037669a7927ee3bab930b031fc7f
AskerOsk <oskar1999asker@gmail.com>
2024-08-12 20:52:19 +0500

    Изменение размера массива

---
+++ Изменение размера массива/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Изменение_размера_массива
+{
+    class Program
+    {
+
+        static void Resize(ref int[] array, int newSize)  // Можно использовать дженерики <T>, для принятия любых типо данных - нужно изучить.
+        {
+            int[] newArray = new int[newSize];
+
+            for (int i = 0; i < array.Length && i < newArray.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] myArray = { 4, 65, 7 };
+            Resize(ref myArray, 10);
+
+          //  Array.Resize(ref myArray, 5);  // Уже есть готовый функционал для изменения размера массива.
+
+        }
+    }
+}
