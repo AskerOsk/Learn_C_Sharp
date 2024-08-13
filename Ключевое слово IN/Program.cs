4d7feed3a0ad5d2cf43cb2c752633030fba890e1
AskerOsk <oskar1999asker@gmail.com>
2024-08-13 19:23:03 +0500

    Ключевое_слово_IN

---
+++ Ключевое слово IN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ключевое_слово_IN
+{
+    class Program
+    {
+
+        static void Foo(in int value) // in только для чтения. Используются для увелечение скорости.
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Foo(a); // Можно без in
+        }
+    }
+}
