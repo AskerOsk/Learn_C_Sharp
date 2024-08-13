9474f238f5983f9363c01860ffbeb02ec840360e
AskerOsk <oskar1999asker@gmail.com>
2024-08-13 14:14:17 +0500

    Удаление элементов из массива

---
+++ УДАЛИТЬ ЭЛЕМЕНТ ИЗ МАССИВА/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace УДАЛИТЬ_ЭЛЕМЕНТ_ИЗ_МАССИВА
+{
+    class Program
+    {
+
+        static void DeleteAt(ref int [] array, int index)  // Удаление элемента массива по указанному индексу.
+        {
+            int[] newArray = new int[array.Length -1];
+
+            for (int i = 0; i < index;  i++)
+            {
+                newArray[i] = array[i];
+            }
+            for (int i = index + 1; i < array.Length; i++)
+            {
+                newArray[i - 1] = array[i];
+            }
+
+            array = newArray;
+        }
+        
+        static void DeleteFirs(ref int[] array) // Удаление первого элемента
+        {
+            DeleteAt(ref array, 0);
+        }
+
+        static void DeleteLast(ref int[] array) // Удаление последнего элемента
+        {
+            DeleteAt(ref array, array.Length - 1);
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] myArray ={ 10, 50, 401, 457, 594 };
+
+            DeleteAt(ref myArray, 2);
+            DeleteFirs(ref myArray);
+            DeleteLast(ref myArray);
+           
+
+        }
+    }
+}
