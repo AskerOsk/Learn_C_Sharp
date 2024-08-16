107be046413e0deeb7ee03f3a8b4abb2f7da2a38
AskerOsk <oskar1999asker@gmail.com>
2024-08-16 17:46:27 +0500

    Вывод элементов с помощью Рекурсии

---
+++ рекурсия/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace рекурсия
+{
+    class Program
+    {
+
+        //static void Foo(int i)
+        //{
+        //    Console.WriteLine(i);
+
+        //    if (i > 3) return;
+        //    i++;
+
+        //    Foo(i);
+
+        //}
+
+        static void PrintArr(int[] myArr, int i = 0)
+        {
+            if (i < myArr.Length)
+            {
+                Console.WriteLine(myArr[i]);
+                PrintArr(myArr, i + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] myArray = { 45, 432, 67 };
+            PrintArr(myArray);
+            // Foo(1);
+        }
+    }
+}
