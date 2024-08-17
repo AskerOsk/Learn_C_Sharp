9d02bb80e402be676beb037016dd82ccce1d37c6
AskerOsk <oskar1999asker@gmail.com>
2024-08-18 00:07:54 +0500

    Что такое Nullable | Null совместимые значимые типы Nullable

--- Пройденные уроки без кода/Program.cs
+++ Пройденные уроки без кода/Program.cs
@@ -10,20 +10,34 @@ namespace Пройденные_уроки_без_кода
                 Class, struct, enum
             */
             Console.WriteLine("ССЫЛОЧНЫЕ И ЗНАЧИМЫЕ ТИПЫ C# | СТЕК И КУЧА C# | REFERENCE AND VALUE TYPES C# | C# Уроки | # 38 \nhttps://www.youtube.com/watch?v=_RUluZNOwfY&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=51");
-
+            Console.WriteLine();
             Console.WriteLine("ОБЛАСТЬ ВИДИМОСТИ | КОНТЕКСТ ПЕРЕМЕННОЙ | КОНФЛИКТЫ ОБЛАСТЕЙ ВИДИМОСТИ | C# Уроки | # 37 \nhttps://www.youtube.com/watch?v=FyCp19W_5d8&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=50");
+            Console.WriteLine();
             Console.WriteLine("ЧТО ТАКОЕ NULL | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 39 \nhttps://www.youtube.com/watch?v=wqUiYYVZDI4&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=52");
+            Console.WriteLine();
             Console.WriteLine("ОПЕРАТОР ОБЪЕДИНЕНИЯ С NULL С# | C# ?? | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 40 \n https://www.youtube.com/watch?v=mXh0ci6O6og&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=54");
+            Console.WriteLine();
             Console.WriteLine("ОПЕРАТОР ПРИСВАИВАНИЯ ОБЪЕДИНЕНИЯ СО ЗНАЧЕНИЕМ NULL в C# 8 | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 41\nhttps://www.youtube.com/watch?v=Jb-dFOXrSa0&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=54");
+            Console.WriteLine();
             Console.WriteLine("ОПЕРАТОР УСЛОВНОГО NULL | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 42 ?. \n https://www.youtube.com/watch?v=UwVYKGi0XJk&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=55");
+            Console.WriteLine();
             Console.WriteLine("КЛЮЧЕВОЕ СЛОВО PARAMS | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ \n https://www.youtube.com/watch?v=gwYuiwelSQs&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=62");
+            Console.WriteLine();
             Console.WriteLine("ЧТО ТАКОЕ РЕКУРСИЯ C# | ПЕРЕПОЛНЕНИЕ СТЕКА | СТЕК ВЫЗОВОВ | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 49 \n https://www.youtube.com/watch?v=B50p1BRyb_E&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=65");
+            Console.WriteLine();
             Console.WriteLine(" Как найти сумму цифр числа | СИ ШАРП ДОМАШНИЕ ЗАДАНИЯ # 17 \n https://www.youtube.com/watch?v=c4h0FKXkWf8&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=68");
+            Console.WriteLine();
             Console.WriteLine("ПРЕОБРАЗОВАНИЕ И ПРИВЕДЕНИЕ ТИПОВ В C# | Явное| Неявное | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 50 \n https://www.youtube.com/watch?v=cfoec_hRfPU&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=69");
+            Console.WriteLine();
+            Console.WriteLine("Что такое Nullable | Null совместимые значимые типы Nullable \n https://www.youtube.com/watch?v=jhz3Ltc09m4&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=71");
+            Console.WriteLine();
             // Console.WriteLine(" \n");
+            Console.WriteLine();
             // Console.WriteLine(" \n");
+            Console.WriteLine();
             // Console.WriteLine(" \n");
-
+            Console.WriteLine();
+            // Console.WriteLine(" \n");
 
 
             /* 
