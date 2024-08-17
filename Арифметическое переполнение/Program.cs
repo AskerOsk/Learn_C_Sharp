e732b2d15fec97d9446c0b1ec54b169d2367a24f
AskerOsk <oskar1999asker@gmail.com>
2024-08-17 23:40:22 +0500

    Арифметическое_переполнение

---
+++ Арифметическое переполнение/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Арифметическое_переполнение
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte a = 1;
+
+            byte b = 2;
+
+
+            //checked
+            //{
+            //    a = (byte)a - b; // показыввает ошибку, так как в настроках включена проверка.  
+            //}
+
+            unchecked
+            {
+                a = (byte)((byte)a - b);  // Убрана проверка на переполнение
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
