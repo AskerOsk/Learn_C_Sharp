f5cd14cdfb9af2f9fec6291aeb5d5bcaff8c90b0
AskerOsk <oskar1999asker@gmail.com>
2024-09-12 20:06:49 +0500

    LeetCode TwoSum, 1684

---
+++ LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+   
+
+    }
+}
