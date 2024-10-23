2d284a9eedaba82e530946710fac98e73fa5944a
Asker Kaudinov <asker.kaudinov@sulpak.kz>
2024-10-23 16:34:24 +0500

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dictionary<int, string> Student = new Dictionary<int, string>
+            //{
+            //    {20 , "Alice"},
+            //    {800 , "Mark"},
+            //    {4010 , "Gong"},
+
+            //};
+            //if (Student.TryGetValue(8100, out string studentName))
+            //{
+            //    Console.WriteLine($"Student name: {studentName}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Значение не найдено");
+
+            //}
+            //Console.ReadLine();
+
+            string text = "hello world hello everyone";
+            string[] words = text.Split(' ');
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            foreach (var kvp in wordCount)
+            {
+                Console.WriteLine($"Word: {kvp.Key}, Count: {kvp.Value}");
+            }
+
+        }
+
+}
+}
