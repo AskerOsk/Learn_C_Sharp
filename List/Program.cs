39fb79e07a3f70aed54a35d3d0b21cd3b2ff3694
Asker Kaudinov <asker.kaudinov@sulpak.kz>
2024-10-23 15:31:18 +0500

    List

---
+++ List/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int> { 13, 50, 546, 87465, 00147, 87410 };
+            num.RemoveAll(x => x % 2 != 0);
+
+            foreach (int x in num) {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
