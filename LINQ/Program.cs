3a0597fd44b3543dc16ac92578e26c40d4aa1338
Asker Kaudinov <asker.kaudinov@sulpak.kz>
2024-10-24 14:14:16 +0500

    Linq

---
+++ LINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Select: Проекция данных.
+        //Where: Фильтрация данных.
+        //OrderBy: Сортировка данных.
+        //GroupBy: Группировка данных.
+        //Join: Соединение двух последовательностей.
+        //Distinct: Удаление дубликатов.
+        //Any: Проверка на наличие хотя бы одного элемента, удовлетворяющего условию.
+        //All: Проверка, удовлетворяют ли все элементы определенному условию.
+        //Count: Подсчет количества элементов.
+        }
+
+
+    }
+}
+
